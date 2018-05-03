6181eb61710701491753280cfe9d48e13e5e812e
paulamp <paulapliegom@gmail.com>
2018-05-03 19:42:35 +0200

    registro de usuarios

---
+++ myproject/museos/views.py
@@ -0,0 +1,38 @@
+import sys
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import csrf_exempt
+from django.template.loader import get_template
+from django.template import Context
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        if not request.user.is_authenticated():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            if not username or not password:
+                return HttpResponseRedirect('/')
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+    return HttpResponseRedirect('/')
+
+def user_logout(request):
+    if request.user.is_authenticated():
+        value = "Logout " + str(request.user.username)
+        logout(request)
+        value += '<br><a href="/">Home</a>'
+    else:
+        value = '<a href="/">Home</a>'
+    return HttpResponse(value)
+
+def notFound(request):
+    value = "Recurso no disponible"
+    value += '<br><a href="/">Home</a>'
+    return HttpResponse(value)
